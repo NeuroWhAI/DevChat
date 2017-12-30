a39816d101b1fe9876863fbfbb429b34264b1e62
neurowhai <tlsehdgus321@naver.com>
2017-12-30 08:02:30 -0600

    Create basic discord bot

--- DevChat/Program.cs
+++ DevChat/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace DevChat
 {
@@ -10,7 +11,28 @@ namespace DevChat
     {
         static void Main(string[] args)
         {
+#if DEBUG
+            using (var sr = new StreamReader("data.txt"))
+            {
+                string token = sr.ReadLine();
+                args = new string[]
+                {
+                    token
+                };
+            }
+#endif
 
+            if (args.Length < 1)
+            {
+                Console.WriteLine("program {token}");
+                return;
+            }
+
+
+            var bot = new DiscordBot(args[0]);
+            //bot.RegisterCommand<>();
+
+            bot.Start();
         }
     }
 }
