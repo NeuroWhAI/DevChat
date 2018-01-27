89f67dca4e9791b4a847c72dfbf353eb89894d3b
NeuroWhAI <tlsehdgus0212@gmail.com>
2018-01-28 08:20:47 +0900

    Add kill command

--- DevChat/BasicCommand.cs
+++ DevChat/BasicCommand.cs
@@ -21,5 +21,40 @@ namespace DevChat
         {
             await ctx.RespondAsync("pong!");
         }
+
+        [Command("kill"), RequireOwner]
+        [Description("kill {Name}\nKill recent {Name} process.")]
+        public async Task Kill(CommandContext ctx, string name)
+        {
+            DateTime recentTime = DateTime.MinValue;
+            Process recentProc = null;
+
+            foreach (var proc in Process.GetProcessesByName(name))
+            {
+                if (proc.StartTime > recentTime)
+                {
+                    recentTime = proc.StartTime;
+                    recentProc = proc;
+                }
+            }
+
+            if (recentProc == null)
+            {
+                await ctx.RespondAsync("There is no process with such a name.");
+            }
+            else {
+                try
+                {
+                    recentProc.Kill();
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e.Message);
+                    Console.WriteLine(e.StackTrace);
+                }
+
+                await ctx.RespondAsync("Process terminated!");
+            }
+        }
     }
 }
